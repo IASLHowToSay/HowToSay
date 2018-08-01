8f53f9d2b84a144d2a7c274db73ab2c726cba7a0
victorlin12345 <victorlin12345@gmail.com>
2018-08-01 17:42:18 +0800

    s_user

---
+++ db/migrations/s_user.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+require 'sequel'
+
+Sequel.migration do
+  change do
+    create_table(:suer) do
+      primary_key :id
+
+      String :name, unique: true, null: false
+      String :email, unique: true, null: false
+      String :password
+      String :role
+
+      DateTime :created_at
+      DateTime :updated_at
+    end
+  end
+end
